c9b0f98040fe2eefa9c60c4ceb6e450d41c4d48e
shelleyn1981-byte <shelleyn1981@gmail.com>
2026-07-14 18:53:19 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.ENTERTAINMENT_CONFIG = {
+  // Paste your deployed Apps Script web-app URL between the quotes.
+  // Leave blank to use fallback-data.js.
+  DATA_ENDPOINT: "https://script.google.com/macros/s/AKfycbxg-3D7kJayqeyV4q_4xVI6shD8J5WzVKcj29FzpWMs7-gFg820qZo7oFbgXsEjk5m0ew/exec",
+  TIME_ZONE: "America/Chicago",
+  AUTO_REFRESH_MINUTES: 30
+};
